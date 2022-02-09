2dee08979aa0f48e94f8e045571fa500fc5e18bc
Richard Wang <rw252@duke.edu>
2022-02-09 16:07:08 -0500

    dictionary and class

--- database.py
+++ database.py
@@ -1,8 +1,39 @@
-def add_patient(patient_name, patient_id, age):
-    new_patient = {"name": patient_name,
-                   "id": patient_id,
-                   "age": age,
-                   "tests": []}
+class Patient:
+
+    def __init__(self, patient_name, patient_id, age, dob=None):
+        self.name = patient_name
+        self.id = patient_id
+        self.age = age
+        self.tests = []
+        self.dob = dob
+
+    def __repr__(self):
+        return "Patient: {}, {}".format(self.name, self.id)
+
+    def output_patient(self):
+        outstring = "Name: {}\n".format(self.name)
+        outstring += "Age {}\n".format(self.age)
+        outstring += "Tests: {}\n".format(self.tests)
+        return outstring
+
+    def is_adult_or_minor(self):
+        if self.age >= 18:
+            return "Adult"
+        else:
+            return "Minor"
+
+    def id_tag_string(self):
+        return "{}: {}".format(self.name, self.id)
+
+    def add_test(self, test_name, test_result):
+        self.tests.append((test_name, test_result))
+
+    def increase_age(number_of_years):
+        self.age += number_of_years
+
+
+def add_patient(patient_name, patient_id, age, dob=None):
+    new_patient = Patient(patient_name, patient_id, age, dob)
     return new_patient
 
 
@@ -10,7 +41,8 @@ def main():
     db = []
     x = add_patient("Ann Ables", 342, 40)
     db.append(x)
-    y = add_patient("Bob Boyles", 50, 50)
+    print(db[0].name)
+    y = add_patient("Bob Boyles", 50, 50, "1/1/1970")
     db.append(y)
     z = add_patient("Chris Columbus", 111, 35)
     db.append(z)
@@ -25,7 +57,7 @@ def main():
 
 def find_patient(db, id):
     for patient in db:
-        if patient["id"] == id:
+        if patient.id == id:
             return patient
     return
 
@@ -43,7 +75,7 @@ def output_patient(patient):
 def add_test_to_patient(db, id, test_name, test_result):
     patient = find_patient(db, id)
     test_tuple = (test_name, test_result)
-    patient["tests"].append(test_tuple)
+    patient.tests.append(test_tuple)
 
 
 def print_directory(db):
@@ -60,5 +92,5 @@ def create_id_tag_string(patient):
 
 if __name__ == "__main__":
     db = main()
-    print_directory(db)
-    print(create_id_tag_string(find_patient(db, 111)))
+    # print_directory(db)
+    # print(create_id_tag_string(find_patient(db, 111)))
